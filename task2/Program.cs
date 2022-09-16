d57658cba21b5d07a7e1cfd56197bd090c477675
Bormental <tyranosaurus@yandex.ru>
2022-09-16 17:29:10 +0300

    Task5 added

--- task2/Program.cs
+++ task2/Program.cs
@@ -5,11 +5,11 @@ System.Console.WriteLine("Ведите номер дня недели: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 if (number == 1) System.Console.WriteLine("Понедельник");
-if (number == 2) System.Console.WriteLine("Вторник");
-if (number == 3) System.Console.WriteLine("Среда");
-if (number == 4) System.Console.WriteLine("Четверг");
-if (number == 5) System.Console.WriteLine("Пятница");
-if (number == 6) System.Console.WriteLine("Суббота");
-if (number == 7) System.Console.WriteLine("Воскресенье");
+else if (number == 2) System.Console.WriteLine("Вторник");
+else if (number == 3) System.Console.WriteLine("Среда");
+else if (number == 4) System.Console.WriteLine("Четверг");
+else if (number == 5) System.Console.WriteLine("Пятница");
+else if (number == 6) System.Console.WriteLine("Суббота");
+else if (number == 7) System.Console.WriteLine("Воскресенье");
 
 
---
+++ task5/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая принимает на вход трехзначное число и на выходе показывает последнюю цифру этого числа.
+456 -> 6
+782 -> 2
+918 -> 8
+*/
+
+System.Console.WriteLine("Введите трехзначное число: ");
+string number = Console.ReadLine();
+System.Console.WriteLine(number[2]);
+
+/* Альтернативная программаЖ:
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Последняя цифра {number % 10}");
