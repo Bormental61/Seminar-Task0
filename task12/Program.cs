5549df182200b7e30da7e7a45e27416f2b40e29f
Bormental <tyranosaurus@yandex.ru>
2022-09-21 21:22:19 +0300

    task12 solved

---
+++ task12/Program.cs
@@ -0,0 +1,21 @@
+﻿/* 
+Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+14 -> нет 
+46 -> нет 
+161 -> да
+*/
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int checkIfDiv7 = number % 7;
+int checkIfDiv23 = number % 23;
+
+if (checkIfDiv7 == 0 && checkIfDiv23 == 0)
+{
+    Console.WriteLine($"Число {number} кратно 7 и 23.");
+}
+else
+{
+    Console.WriteLine($"Число {number} не является кратным одновременно 7 и 23.");
+}
