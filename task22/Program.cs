57c4c1ca840c16d7e66cb6242ca4119eb363795c
Bormental <tyranosaurus@yandex.ru>
2022-09-24 18:17:42 +0300

    task22 added

---
+++ task22/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4
+*/
+int getNumberFromUser(string userInformation)
+{
+    int result = 0;
+    while (result == 0)
+    {
+        Console.Write(userInformation);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine, out result);
+        if (result == 0 && userLine != "0")
+        {
+            Console.WriteLine($"Введите целое число, вы ввели {userLine}");
+        }
+        else
+        {
+            break;
+        }
+    }
+    return result;
+}
+Console.Write("Введите целое число больше 0: ");
+int userNumber = getNumberFromUser(" ");
+int squareNumber = 1;
+while (squareNumber <= userNumber)
+{
+    Console.Write($"{Math.Pow(squareNumber, 2)} ");
+    squareNumber++;
+}
