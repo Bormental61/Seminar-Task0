a8c1ccda58d764f563d85058ffe709ead7390ff2
Bormental <tyranosaurus@yandex.ru>
2022-09-28 20:19:12 +0300

    task28 added

---
+++ task28/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24
+5 -> 120
+*/
+
+void printInConsoleWithColor(string information, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.WriteLine(information);
+    Console.ResetColor();    
+}    
+int getFactorialOfNumber(int startPoint, int endPoint)
+{
+    int result = 1;
+    if (startPoint > endPoint)
+    {
+        printInConsoleWithColor($"Верхняя граница отрицательна {endPoint}", ConsoleColor.DarkYellow);
+        int buff = startPoint;
+        startPoint = endPoint;
+        endPoint = buff;
+    }
+    for (int i = startPoint; i <= endPoint; i++)
+    {
+        result *= i;
+    }
+    return result;  
+}
+printInConsoleWithColor("Введите верхнюю границу для суммы ряда", ConsoleColor.DarkMagenta);
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int sumOfRange = getFactorialOfNumber(1, userNumber);
+Console.WriteLine($"Произведение чисел от 1 до {userNumber} = {sumOfRange}");
