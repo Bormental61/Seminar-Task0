c11b6cbae74dbd7e559b4ed37632ad2cfd42c357
Bormental <tyranosaurus@yandex.ru>
2022-10-09 16:56:23 +0300

    task46 solved

---
+++ task46/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+Задача 46. 
+Задайте двумерный массив размером m x n,
+заполненный случайными целыми числами.
+m=3, n=4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1
+*/
+
+int[,] Generate2DArray(int height, int width, int randomStart, int randomEnd)
+{
+    int[,] twoDArray = new int[height, width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            twoDArray[i, j] = new Random().Next(randomStart, randomEnd + 1);
+        }
+    }
+    return twoDArray;
+}
+void PrintColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+void print2DArray(int[,] arrayToPrint)
+{
+    Console.Write("  ");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        PrintColorData(i + "\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        PrintColorData(i + " ");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i, j] + "\t");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+}
+
+int[,] generatedArray = Generate2DArray(7, 5, 1, 100);
+print2DArray(generatedArray);
