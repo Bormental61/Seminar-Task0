0ce3529ac19b2ef3181c118455e1a72269ba6faa
Bormental <tyranosaurus@yandex.ru>
2022-10-09 17:21:23 +0300

    task48 solved

---
+++ task48/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5
+*/
+int[,] GetIndexSummedArray(int height, int width)
+{
+    int[,] indexSummedArray = new int[height, width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            indexSummedArray[i,j] = i + j;
+        }
+    }
+    return indexSummedArray;
+}
+void PrintColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+}
+void Print2DArray(int[,] arrayToPrint)
+{
+    Console.Write("  ");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        PrintColorData(i + "\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        PrintColorData(i + " ");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i, j] + "\t");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+}
+
+int[,] newIndexSummedArray = GetIndexSummedArray(3,4);
+Print2DArray(newIndexSummedArray);
