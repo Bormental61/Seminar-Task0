7f3ca2de5f017fad88f7f51650fda3b93c61ee69
Bormental <tyranosaurus@yandex.ru>
2022-10-19 00:23:04 +0300

    task 65, 67 added

---
+++ task65/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+M = 4; N = 8 -> "4, 6, 7, 8"
+*/
+
+Console.WriteLine("Input start number M");
+int userNumberStart = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input End number N");
+int userNumberEnd = Convert.ToInt32(Console.ReadLine());
+
+PrintRange(userNumberStart, userNumberEnd);
+
+void PrintRange(int Start, int End)
+{
+    if (End < Start)
+    {
+        return;
+    }
+    PrintRange(Start, End - 1);
+    Console.Write($"{End}, ");
+}
---
+++ task67/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+
+Console.WriteLine("Input number N");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(GetSum(userNumber));
+
+int GetSum(int N)
+{
+    if (N == 0)
+    {
+        return 0;
+    }
+    int digit = N % 10;
+    return digit + GetSum(N / 10);
+}
